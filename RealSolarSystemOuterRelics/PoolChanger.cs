58d36d0e4b48044fe8dfd05d60c45f67b6342c02
GameWyrm <daronan97@hotmail.com>
2023-02-15 14:42:20 -0700

    Created Project

---
+++ RealSolarSystemOuterRelics/PoolChanger.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+
+namespace RealSolarSystemOuterRelics
+{
+    /// <summary>
+    /// This class handles changing the pools available to Outer Relics
+    /// </summary>
+    public class PoolChanger
+    {
+        // First we should grab our mod's singleton
+        RealSolarSystemOuterRelics main => RealSolarSystemOuterRelics.Instance;
+        // This is a delegate. It tells the code that whenever the main field is referenced, to instead look at our singleton
+        // For convenience, let's also make a reference to the API here
+        IOuterRelicsAPI orAPI => main.orAPI;
+
+        // Let's also create the lists that will be used the options
+        // We'll need a constructor for this
+        // Lists are part of System.Collections.Generic. If you don't want to use a List for whatever reason, you can use an array instead.
+        List<string> closePlanets;
+        List<string> distantPlanets;
+        public PoolChanger()
+        {
+            closePlanets = new List<string>
+            {
+                "Earth_Body",
+                "Europa_Body",
+                "HalleysComet_Body",
+                "Mars_Body",
+                "Mercury_Body",
+                "Pluto_Body",
+                "TheMoon_Body",
+                "Titan_Body",
+                "Titania_Body",
+                "Triton_Body",
+                "Venus_Body"
+            };
+            distantPlanets = new List<string>
+            {
+                "BarnardsB_Body",
+                "Perdition_Body",
+                "ProximaB_Body"
+            };
+        }
+        
+        /// <summary>
+        /// Enables or disables planets orbitting Sol
+        /// </summary>
+        /// <param name="enable">If true, the files will be registerd, or unregistered otherwise.</param>
+        public void SetClosePlanets(bool enable)
+        {
+            bool success;
+            if (enable)
+            {
+                // Here we register all of the close planets. The false is unnecessary but helps makes this clear.
+                success = orAPI.TryRegisterFiles(main, closePlanets, false);
+            }
+            else
+            {
+                success = orAPI.TryUnregisterFiles(main, closePlanets, false);
+            }
+
+            // You may have noticed that TryRegisterFiles and TryUnregisterFiles are bools. They will pass true if they succeed, or false otherwise.
+            // This allows you to run code based on whether they succeed if you want. This is optional and you will see that we do not bother for the distant planets.
+            // This is a bit of a complex output log that simply tells us whether the list was turned on or off, but only if it succeeds
+            if (success) main.ModHelper.Console.WriteLine("Toggled close planets! They are now " + (enable ? "On" : "Off"));
+        }
+
+        /// <summary>
+        /// Enables or disables planets in distant solar systems
+        /// </summary>
+        /// <param name="enable">If true, the files will be registered, or unregisterd otherwise.</param>
+        public void SetDistantPlanets(bool enable)
+        {
+            if (enable)
+            {
+                orAPI.TryRegisterFiles(main, distantPlanets, false);
+            }
+            else
+            {
+                orAPI.TryUnregisterFiles(main, distantPlanets, false);
+            }
+        }
+
+        /// <summary>
+        /// Enables or disables hints spawning in Real Solar System
+        /// </summary>
+        /// <param name="enable">If true, the hints will be enabled, or disabled otherwise.</param>
+        public void SetCustomHints(bool enable)
+        {
+            // This function is very similar to registering placement files,
+            // but you set the "isHint" variable to true, which will load the file from your Hints folder
+            // We only have one hint file, so we use the singular version of the function
+            if (enable)
+            {
+                orAPI.TryRegisterFile(main, "HintPlacements", true);
+            }
+            else
+            {
+                orAPI.TryUnregisterFile(main, "HintPlacements", true);
+            }
+        }
+    }
+}
---
+++ RealSolarSystemOuterRelics/RealSolarSystemOuterRelics.cs
@@ -0,0 +1,111 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace RealSolarSystemOuterRelics
+{
+    public class RealSolarSystemOuterRelics : ModBehaviour
+    {
+        // Here is one way to create a mod Singleton
+        // First you create a static variable that is an instance of your main mod class
+        private static RealSolarSystemOuterRelics instance;
+
+        // Then you create a property that references it
+        public static RealSolarSystemOuterRelics Instance
+        {
+            get
+            {
+                if (instance == null) instance = FindObjectOfType<RealSolarSystemOuterRelics>();
+                return instance;
+            }
+        }
+        // In other classes, you can reference the singleton using code like this:
+        // RealSolarSystemOuterRelics Main = RealSolarSystemOuterRelics.Instance;
+
+        // You'll then want to make a field to hold the API. It does not need to be public to work, but we make it public here to reference in another class.
+        public IOuterRelicsAPI orAPI;
+
+        // We're also going to create some fields to determine which settings we have enabled
+        bool closePlanets;
+        bool distantPlanets;
+        bool useHints;
+
+        // Since we handle pool management in a seperate class, we'll also want a reference to it
+        PoolChanger pools;
+
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+            // This includes anything having to do with Outer Relics
+        }
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+
+            // You'll want to get the Outer Relics API early
+            orAPI = ModHelper.Interaction.TryGetModApi<IOuterRelicsAPI>("GameWyrm.OuterRelics");
+
+            // The easiest way to register your mod is to register all the files in it at once, like this:
+
+            // orAPI.RegisterMod(this);
+
+            // RegisterMod adds all the files from your mod into the randomization pool, including hint placements.
+            // You need to specify your mod, however, so Outer Relics can keep track of it.
+            // If you register in your main mod class, the "this" keyword will suffice. If using a different class, you'll need to get your mod class' singleton.
+            // However, doing this provides you no control over which files are used. If you use a custom config to let the player decide where items and hints can spawn, you'll need to register files manually
+            // We do this in the Configure method below
+
+            // But before we do that, let's load our settings from the config file and register files as needed
+            closePlanets = ModHelper.Config.GetSettingsValue<bool>("ClosePlanets");
+            distantPlanets = ModHelper.Config.GetSettingsValue<bool>("DistantPlanets");
+            useHints = ModHelper.Config.GetSettingsValue<bool>("Hints");
+
+            // Before we can manage pools, we'll need to instantiate our pool manager
+            pools = new();
+
+            // Now let's use our pool manager to change existing pools
+            if (closePlanets) pools.SetClosePlanets(true);
+            if (distantPlanets) pools.SetDistantPlanets(true);
+            if (useHints) pools.SetCustomHints(true);
+
+            // You should also register the proper names of any bodies in your mod
+            // Any body with a standard name will work fine
+            // For example, if you have a body named "MyCoolPlanet_Body" then Outer Relics will automatically give it the name "My Cool Planet"
+            // However, you may want to define a custom name
+            // For example, you may have called MyCoolPlanet_Body "Super Coolio"
+            // In this case, you can set its name here, like this
+            // orAPI.RegisterBody("MyCoolPlanet_Body", "Super Coolio");
+            // Here we do it for bodies that have special characters
+            orAPI.RegisterBody("BarnardsB_Body", "Barnard's B");
+            orAPI.RegisterBody("HalleysComet_Body", "Halley's Comet");
+        }
+
+        // This method runs whenever the Mod Config is edited in-game
+        // Here we use it to register and unregister files as needed
+        public override void Configure(IModConfig config)
+        {
+            if (pools != null)
+            {
+                if (config.GetSettingsValue<bool>("ClosePlanets") != closePlanets)
+                {
+                    closePlanets = config.GetSettingsValue<bool>("ClosePlanets");
+                    pools.SetClosePlanets(closePlanets);
+                }
+
+                if (config.GetSettingsValue<bool>("DistantPlanets") != distantPlanets)
+                {
+                    distantPlanets = config.GetSettingsValue<bool>("DistantPlanets");
+                    pools.SetDistantPlanets(distantPlanets);
+                }
+                if (config.GetSettingsValue<bool>("Hints") != useHints)
+                {
+                    useHints = config.GetSettingsValue<bool>("Hints");
+                    pools.SetCustomHints(useHints);
+                }
+            }
+        }
+
+    }
+}
